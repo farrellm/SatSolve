ffa58ddda3fadf943c5ab2d9cee5faeb184f9927
Matthew Farrell <farrellm23@gmail.com>
2011-12-05 02:40:23 -0500

    fix gcd

--- math/Rational.ml
+++ math/Rational.ml
@@ -3,13 +3,16 @@ module Rational =
     type field = int * int
 
     let rec gcd a b =
+      if a<0 then gcd (-a) b else
+      if b<0 then gcd a (-b) else
       if a>b then gcd b a else
         let m = b mod a in
           if m=0 then a else gcd m a
 
     let make n d =
-      let g = gcd n d in
-      (n/g),(d/g)
+      if n==0 then 0,1 else
+        let g = gcd n d in
+        (n/g),(d/g)
 
     let plus a b =
       make ((fst a)*(snd b) + (fst b)*(snd a)) ((snd a)*(snd b))
@@ -22,5 +25,7 @@ module Rational =
     let inverse a = if (fst a)>0 then (snd a),(fst a) else -(snd a),-(fst a)
     let one = 1,1
 
-    let print a = print_int (fst a) ; print_string "/" ; print_int (snd a)
+    let print a =
+      if (snd a)==1 then print_int (fst a) else
+        (print_int (fst a) ; print_string "/" ; print_int (snd a))
   end
